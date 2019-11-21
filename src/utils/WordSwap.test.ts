80d2c5c59a49c71f3a9ffad1c6c5c4529235cb1c
Alexander Buczynsky <alexander.buczynsky@siemens.com>
2019-11-21 14:19:28 -0600

    added wordswap test

---
+++ src/utils/WordSwap.test.ts
@@ -0,0 +1,31 @@
+import { WordSwap } from "./WordSwap";
+
+describe("WordSwap", () => {
+  it("should swap the words in the buffer", () => {
+
+    const byte0 = 0;
+    const byte1 = 255;
+    const byte2 = 128;
+    const byte3 = 4;
+
+    const originalBuffer = Buffer.from(
+      new Uint8Array([
+        byte0,
+        byte1,
+        byte2,
+        byte3,
+      ])
+    )
+
+    const originalWord0 = originalBuffer.slice(0, 1);
+    const originalWord1 = originalBuffer.slice(2, 3);
+
+    const swappedBuffer = WordSwap(originalBuffer);
+
+    const swappedWord0 = swappedBuffer.slice(0, 1);
+    const swappedWord1 = swappedBuffer.slice(2, 3);
+
+    expect(originalWord0).toEqual(swappedWord1)
+    expect(originalWord1).toEqual(swappedWord0)
+  })
+})
